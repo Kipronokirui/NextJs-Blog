a822d0a1571286bbf02edaa8a96740e06bad2f0b
Kipronokirui <victorkipronokirui2@gmail.com>
2023-06-25 01:58:06 +0300

    Posts Listing

--- components/PostWidget.js
+++ components/PostWidget.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import moment from 'moment';
+import Link from 'next/link';
+import { getSimilarPosts, getRecentPosts } from '../services';
 
-const PostWidget = () => {
+const PostWidget = ({ categories, s }) => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
+    useEffect(() => {
+        if (s) {
+          getSimilarPosts(categories, s).then((result) => {
+            setRelatedPosts(result);
+          });
+        } else {
+          getRecentPosts().then((result) => {
+            setRelatedPosts(result);
+          });
+        }
+      }, [s]);
   return (
-    <div>PostWidget</div>
+      <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+              {s ? 'Related Posts' : 'Recent Posts'}
+          </h3>
+      </div>
   )
 }
 
--- services/index.js
+++ services/index.js
@@ -38,3 +38,45 @@ export const getPosts = async () => {
 
       return result.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+    const query = gql`
+      query GetPostDetails() {
+        posts(
+          orderBy: createdAt_ASC
+          last: 3
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          s
+        }
+      }
+    `;
+    const result = await request(graphqlAPI, query);
+  
+    return result.posts;
+};
+  
+export const getSimilarPosts = async (categories, s) => {
+    const query = gql`
+      query GetPostDetails($slug: String!, $categories: [String!]) {
+        posts(
+          where: {s_not: $s, AND: {categories_some: {s_in: $categories}}}
+          last: 3
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          s
+        }
+      }
+    `;
+    const result = await request(graphqlAPI, query, { s, categories });
+  
+    return result.posts;
+  };
